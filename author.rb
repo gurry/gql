f2bf21fc8bf0f0dcbe148c05c65959e1c489ccbd
Gurinder Singh <gurinder.singh@1e.com>
2013-09-19 18:50:57 +0530

    Added some code in the previously hollow Author class

--- author.rb
+++ author.rb
@@ -1,6 +1,13 @@
 require 'git'
 
 class Author < Git::Author
+	def initialize(base, author_string)
+		@base = base
+		super(author_string)
+	end
+
 	def commits()
+		an_impossibly_large_max_count = 1000000 # Hack to get ALL commits in the repo because it's mandator to pass a max-count to ruby-git
+		@base.log(an_impossibly_large_max_count).author(name)
 	end
 end
